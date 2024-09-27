18dd50527b4a3337c9e3fc7021d5a29a3558e022
hov8aa <hov8aa@gmail.com>
2024-09-27 12:44:38 +0530

    added comments to resolve the next steps of operation

--- MarsRover.rb
+++ MarsRover.rb
@@ -10,6 +10,15 @@ class MarsRovers
     end
 end
 
+#Now that we know, where is the rover on the grid, we need to do 2 activities here
+# 1. Validate if the input is valid
+    #def  Validate, what does this default method do?
+    #end
+
+# 2. Separte the into 3 parts:
+#    x coordinate, y coordinate & the direction where the rover faces.
+#   how to separate the user input in 3 different variables?
+
 def get_user_input
 
     puts"Welcome to Mars, let's deign the game, enter the size of the grid you want to play with"
