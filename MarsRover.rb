f707e8656c4f06665c6153f344c74c9ae4e56afa
hov8aa <hov8aa@gmail.com>
2024-09-27 12:18:51 +0530

    Updated the get_user_input function to take input as the location of the rover on the grid, which we will need to verify if the location lies on the grid and repeat the previous step to get a valid input.

--- MarsRover.rb
+++ MarsRover.rb
@@ -2,7 +2,7 @@ class MarsRovers
     #defining the directions to move the rover in clock wise direction
     DIRECTIONS = ['N','W','S','E']
 
-    #initialize a new rover with current coordinates
+    #initialize a new rover object with current coordinates
     def initialize (x,y,direction)
         @X = x
         @Y = y 
@@ -15,6 +15,10 @@ def get_user_input
     puts"Welcome to Mars, let's deign the game, enter the size of the grid you want to play with"
     grid = gets
     puts "I hope the playground is big enough #{grid}"
+
+    puts "Now, assume that you have the opportunity to place the rover anywhere on this grid, where would you place the rover & facing which direction?"
+    position = gets
+    puts "I am assuming the position lies on the grid, #{position} (we'll check soon)"
 
 end
 
