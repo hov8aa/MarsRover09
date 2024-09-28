1b639f658133f767774a84e8b414e56356695de5
hov8aa <hov8aa@gmail.com>
2024-09-28 13:05:16 +0530

    Now, we are directly taking the user input into 3 different variables

--- MarsRover.rb
+++ MarsRover.rb
@@ -18,24 +18,9 @@ end
     #Learning to validate an input
 #   After a break, I am realizing that, the validation part need to be a second priority.
 
-# 2. Separte the into 3 parts:
-#    x coordinate, y coordinate & the direction where the rover faces.
-#   how to separate the user input in 3 different variables?
+#   Following code should create a new MarsRovers object with given variables
+#marsrover = MarsRovers.new (rover_position_x_coordinate,rover_position_y_coordinate,rover_facing_direction)
 
-# Following code should create a new MarsRovers object with given variables
-# I learnt that "attr_accessor" can help break down the user input. Need more details.
-# Distractedly learning attr_accessor
-
-#attr_accessor :position somewhere holds the key, But I need to go to the sabji mandi.
-
-#Starting the day to lear to code 12:15
-#Need to ask again, how to separate the user input in different variables? 
-#(do I really need to, is also an important question because maybe the language provides a way to handle this without that)
-#
-
-#x,y,@direction = gets.chomp.split MarsRovers.new
-
-#rover_position = MarsRovers.new (x,y,direction)
 #puts "The System figured that the original position of the rover is: #{@x}, #{@y} & is facing #{@direction} direction"
 
 def get_user_input
@@ -44,9 +29,13 @@ def get_user_input
     grid = gets
     puts "I hope the playground is big enough #{grid}"
 
-    puts "Now, assume that you have the opportunity to place the rover anywhere on this grid, where would you place the rover?"
-    rover_position_coordinates = gets
-    puts "I am assuming the position lies on the grid, #{rover_position_coordinates} (we'll check soon)"
+    puts "Now, assume that you have the opportunity to place the rover anywhere on this grid, where would you place the rover, on X coordinate?"
+    rover_position_x_coordinate = gets.chomp
+
+    puts "Now, assume that you have the opportunity to place the rover anywhere on this grid, where would you place the rover, on Y coordinate?"
+    rover_position_y_coordinate = gets.chomp
+
+    puts "I am assuming the position lies on the grid, #{rover_position_x_coordinate},#{rover_position_y_coordinate} (we'll check soon)"
 
     puts "Wait a second, we missed, What direction the rover is facing, East/West/North/South?"
     rover_facing_direction = gets
