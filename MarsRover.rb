e817e184a733539398d46e86ed8eaaa21cd67f39
hov8aa <hov8aa@gmail.com>
2024-09-28 13:32:25 +0530

    We've taken the player input in the form of a series on instructions in a string, Now we need to process the string to move the rover.

--- MarsRover.rb
+++ MarsRover.rb
@@ -1,19 +1,16 @@
-class MarsRovers
-    #defining the directions to move the rover in clock wise direction
-    DIRECTIONS = ['N','W','S','E']
+#class MarsRovers
+#    defining the directions to move the rover in clock wise direction
+#    DIRECTIONS = ['N','W','S','E']
 
-    #initialize a new rover object with current coordinates
-    def initialize (x,y,direction)
-        @X = x
-        @Y = y 
-        @direction = direction
-    end
-
-    marsrover = MarsRovers.new (rover_position_x_coordinate , rover_position_y_coordinate, rover_facing_direction)
-
-    #puts "The System figured that the original position of the rover is: #{rover_position_x_coordinate}, #{rover_position_y_coordinate} & is facing #{rover_facing_direction} direction"
-
-end
+#    #initialize a new rover object with current coordinates
+#    def initialize (x,y,direction)
+#        @X = x
+#        @Y = y 
+#        @direction = direction
+#    end
+#    marsrover = MarsRovers.new (rover_position_x_coordinate , rover_position_y_coordinate, rover_facing_direction)
+#    puts "The System figured that the original position of the rover is: #{rover_position_x_coordinate}, #{rover_position_y_coordinate} & is facing #{rover_facing_direction} direction"
+#end
 
 #Now that we know, where is the rover on the grid, we need to do 2 activities here
 # 1. Validate if the input is valid
@@ -43,7 +40,16 @@ def get_user_input
     puts "Wait a second, we missed, What direction the rover is facing, East/West/North/South?"
     rover_facing_direction = gets
     rover_facing_direction = rover_facing_direction[0]
-    puts "Direction starts with #{rover_facing_direction}"
+    puts "Hmm, Direction starts with #{rover_facing_direction}"
+
+    #Now we need to get the instructions from the player for further operation
+    puts "Please enter the instructions for the rover to move with 3 characters 'L','M' &'R' in any required sequence where 'L' and 'R' makes the rover spin 90 degrees left or right respectively, without moving from its current spot. 'M' means move forward one grid point, and maintain the same heading."
+    movement = gets.chomp
+
+    puts "Let me move the rover with #{movement} instructions, & give you the final position, please wait..."
+
+    #Now, process the instructions to move the rover
+
 
 end
 
