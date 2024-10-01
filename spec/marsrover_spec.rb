03799df0999ac6a61910186ebc81ce9fac47b58c
hov8aa <hov8aa@gmail.com>
2024-10-01 14:30:07 +0530

    not able to validate my hypothesis

--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -9,5 +9,13 @@ describe "#get_user_input" do
 
 end
 
-#Hypothesis: It's possible to write the test cases with rspec with a function in base file without taking arguments as inputs.
-#Current Query: how to test a fucntion that doesn't take arguments in ruby specs
+# Hypothesis: It's possible to write the test cases with rspec with a function in base file without taking arguments as inputs.
+# Current Query: how to test a fucntion that doesn't take arguments in ruby specs.
+# New Learning: "expect" is the key to resolve this situation.
+# Hypothetical statement would be something like this:
+
+# Input coortinates & location : 1,2,N
+# Output coordinates & location : 1,3,N
+# expect(input location coordinates & direction).to eq(1,3,N)
+
+# expect(1,2,N).to eq(1,3,N) (to be very specific)
